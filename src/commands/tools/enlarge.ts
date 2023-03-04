8051194e6a4acee20bd254e2c9f444bb85876ec9
D3ord3NidAm <georgeneedham7@gmail.com>
2023-03-04 02:39:54 +0000

    feat: new user command: see users pfp in chat

---
+++ src/commands/tools/enlarge.ts
@@ -0,0 +1,14 @@
+import { publish } from '#handler';
+import { commandModule, CommandType } from '@sern/handler';
+
+export default commandModule({
+	type: CommandType.CtxUser,
+	plugins: [publish()],
+	description: 'Sends users profile image.',
+	execute: async (ctx) => {
+		ctx.reply({
+			ephemeral: true,
+			content: `${ctx.targetUser.displayAvatarURL({ size: 1024 })}`,
+		});
+	},
+});
